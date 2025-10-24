95323336caaabd66842a8b9c9c02ada36e94f6fb
sriram <ss2d22@soton.ac.uk>
2025-10-24 14:29:34 +0100

    added a struct to represent user's compliance data

--- contracts/safestake-registry/src/lib.rs
+++ contracts/safestake-registry/src/lib.rs
@@ -11,3 +11,31 @@ use core::fmt::Debug;
 
 // This should act as user's unique identifier (32 byte hash)
 type IdentityHash = [u8; 32];
+
+// Represents a user's compliance data for responsible gambling
+#[derive(Serial, DeserialWithState)]
+#[concordium(state_parameter = "S")]
+pub struct UserCompliance<S = StateApi> {
+    // Hash of user's identity (will be derived from AccountAddress)
+    pub identity_hash: IdentityHash,
+    // Maximum CCD allowed to spend per day (in microCCD) (will be changed to a stablecoin if i have time later on)
+    //TODO: update contract to use stablecoin instead of CCD
+    pub daily_limit: Amount,
+    // Maximum CCD allowed to spend per month (in microCCD)  
+    //TODO: update contract to use stablecoin instead of CCD
+    pub monthly_limit: Amount,
+    // Amount spent today (resets daily)
+    pub daily_spent: Amount,
+    // Amount spent this month (resets monthly)
+    pub monthly_spent: Amount,
+    // Timestamp of last daily reset
+    pub last_reset_day: Timestamp,
+    // Timestamp of last monthly reset
+    pub last_reset_month: Timestamp,
+    // cooldown period (user is excluded until this time)
+    pub cooldown_until: Option<Timestamp>,
+    // Set of platform IDs where user has gambled
+    pub platforms_used: StateSet<String, S>,
+    // Age verification status
+    pub age_verified: bool,
+}
